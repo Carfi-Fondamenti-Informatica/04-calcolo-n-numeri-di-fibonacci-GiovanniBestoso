1c9900b80707d5b630e98302e558baf34d21ed9f
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-11-22 09:52:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    int n=0, primo=1, secondo=1, terzo=0;
+    cin>>n;
+    if (n>=2){
+        cout<<primo<<endl<<secondo<<endl;
+        for (int i = 0; i < (n-2); ++i) {
+            terzo=primo+secondo;
+            cout<<terzo<<endl;
+            primo=secondo;
+            secondo=terzo;
+        }
+    }else{
+        cout<<"errore"<<endl;
+    }
    return 0;
 }
